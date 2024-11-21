30cd1d96b9f7bcbefbdc8a32f7793e0cf4a3dc45
xkartana <hungykl1605@gmail.com>
2024-11-22 02:16:49 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import cuhklogo from './cuhklogo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={cuhklogo} className="App-logo" alt="logo" />
+        CUHK Forum
       </header>
+      <p>
+        Test B.
+      </p>
     </div>
   );
 }
