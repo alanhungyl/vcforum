eb31aeaf80d0c74863df25f3c9e79018f3fd25e3
xkartana <hungykl1605@gmail.com>
2024-11-22 04:25:07 +0800

    Added tailwind forum name & logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import cuhklogo from './cuhklogo.png';
+import cuhklogo from './icons/cuhklogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={cuhklogo} className="App-logo" alt="logo" />
-        CUHK Forum
+      <header className="App-header bg-blue-500 text-white p-4">
+        <img src={cuhklogo} className="App-logo h-30" alt="logo" />
+        <p className="custom-purple font-bold">CUHK Forum</p>
       </header>
-      <p>
-        Test B.
-      </p>
+      <p>Test B.</p>
     </div>
   );
 }
