6b5f22b2474fd2c2150ab0d0a3ae81739ff4d500
xkartana <hungykl1605@gmail.com>
2024-11-23 03:22:36 +0800

    finished the UI design

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-gray-100">
-      <header className="bg-gray-800 text-white p-4 flex justify-between items-center w-full">
+    <div className="min-h-screen background-dark text-gray-100">
+      <header className="block-background-color text-white p-4 flex justify-between items-center w-full">
         <div className="flex items-center">
           <img src={cuhklogo} className="h-16 p-1 ml-8 mr-8" alt="logo" />
           <p className="custom-purple font-bold text-4xl mr-auto">CUHK Forum</p>
@@ -19,7 +19,7 @@ function App() {
           <input
             type="text"
             placeholder="Type here to search..."
-            className="bg-gray-700 p-4 pl-10 rounded-xl text-xl h-14 w-full"
+            className="bar-background-color p-4 pl-10 rounded-xl text-xl h-14 w-full"
           />
           <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 h-6 w-6" alt="Search" />
         </div>
@@ -32,10 +32,10 @@ function App() {
       </header>
 
       {/* Main Content */}
-      <div className="container p-4 flex justify-between gap-20 mx-auto w-full">
+      <div className="p-4 flex justify-between gap-20 mx-auto ">
         {/* Left Sidebar */}
-        <div className="w-1/4 text-xl mt-12">
-          <div className="bg-gray-800 rounded-2xl p-8 mb-20">
+        <div className="w-1/6 text-xl mt-12 ml-24">
+          <div className="block-background-color rounded-2xl p-8 mb-20">
             <div className="flex items-center space-x-2 mb-6">
               <img src={UserIcon} alt="User Icon" className="w-12 h-12" />
               <div>
@@ -60,7 +60,7 @@ function App() {
           </div>
 
           {/* Hot Topics */}
-          <div className="bg-gray-800 rounded-2xl p-8">
+          <div className="block-background-color rounded-2xl p-8">
             <h2 className="font-bold text-3xl mb-12">Hot Topics</h2>
             <div className="space-y-6">
               <div>
@@ -92,56 +92,61 @@ function App() {
         </div>
 
         {/* Main Content Area */}
-        <div className="text-xl w-full">
+        <div className="text-xl w-4/6">
           {/* Posts */}
-          <div className="bg-gray-800 rounded-2xl p-8 my-12">
-            <p className="font-bold mb-4">Title</p>
-            <p className="text-gray-300 mb-4">Content ...</p>
-            <p className="text-right text-gray-400">Posted by XXXX</p>
+          <div className="block-background-color rounded-2xl p-8 my-12 h-64 relative">
+            <p className="font-bold mb-4">Dummy Title 1</p>
+            <p className="text-gray mb-4">Dummy Content</p>
+            <p className="text-gray-400 absolute bottom-8 right-8">Posted by Test</p>
           </div>
 
-          <div className="bg-gray-800 rounded-2xl p-8 my-12">
-            <p className="font-bold mb-4">Title</p>
-            <p className="text-gray-300 mb-4">Content ...</p>
-            <p className="text-right text-gray-400">Posted by XXXX</p>
+          <div className="block-background-color rounded-2xl p-8 my-12 h-64 relative">
+            <p className="font-bold mb-4">I love CUHK</p>
+            <p className="text-gray mb-4">
+              Founded in 1963, The Chinese University of Hong Kong (CUHK) is a forward-looking comprehensive research university with a global vision and a mission to combine tradition with modernity, and to bring together China and the West. CUHK teachers and students hail from all around the world. CUHK graduates are connected worldwide through an extensive alumni network.`
+            </p>
+            <p className="text-gray-400 absolute bottom-8 right-8">Posted by CUHK Lover</p>
           </div>
-          
-          <div className="bg-gray-800 rounded-2xl p-8 my-12">
-            <p className="font-bold mb-4">Title</p>
-            <p className="text-gray-300 mb-4">Content ...</p>
-            <p className="text-right text-gray-400">Posted by XXXX</p>
+
+          <div className="block-background-color rounded-2xl p-8 my-12 h-64 relative">
+            <p className="font-bold mb-4">I love IERG</p>
+            <p className="text-gray mb-4">
+              In this ever-changing information-centric world, data has enormous hidden value, which can be unlocked by advanced engineering techniques to create an impact on society.
+              Through data generation and measurement, data transmission and networking to connect people and objects, coupled with advanced data processing and analytics, data can be enhanced into forward-looking information and predictions. Together with the added value of network security, we can realize many useful Internet and artificial intelligence applications, and achieve the goal of a smart world.
+            </p>
+            <p className="text-gray-400 absolute bottom-8 right-8">Posted by IERG Lover</p>
           </div>
-          
-          <div className="bg-gray-800 rounded-2xl p-8 my-12">
-            <p className="font-bold mb-4">Title</p>
-            <p className="text-gray-300 mb-4">Content ...</p>
-            <p className="text-right text-gray-400">Posted by XXXX</p>
+
+          <div className="block-background-color rounded-2xl p-8 my-12 h-64 relative">
+            <p className="font-bold mb-4">Dummy Title 2</p>
+            <p className="text-gray mb-4">Content ...</p>
+            <p className="text-gray-400 absolute bottom-8 right-8">Posted by XXXX</p>
           </div>
-          
-          <div className="bg-gray-800 rounded-2xl p-8 my-12">
-            <p className="font-bold mb-4">Title</p>
-            <p className="text-gray-300 mb-4">Content ...</p>
-            <p className="text-right text-gray-400">Posted by XXXX</p>
+
+          <div className="block-background-color rounded-2xl p-8 my-12 h-64 relative">
+            <p className="font-bold mb-4">Dummy Title 3</p>
+            <p className="text-gray mb-4">Content ...</p>
+            <p className="text-gray-400 absolute bottom-8 right-8">Posted by XXXX</p>
           </div>
         </div>
 
         {/* Right Sidebar */}
-        <div className="w-1/4">
-          <div className="bg-gray-800 rounded-2xl p-4">
-            <div className="space-y-4">
+        <div className="w-1/6 mr-24">
+          <div className="block-background-color rounded-2xl p-4 mt-12 text-xl">
+            <div className="space-y-6">
               <div>
-                <p className="text-gray-400">Your Posts</p>
-                <p className="font-bold">0</p>
+                <p className="font-bold">Your Posts</p>
+                <p className="text-gray">0</p>
               </div>
               <div>
-                <p className="text-gray-400">Received Comments</p>
-                <p className="font-bold">0</p>
+                <p className="font-bold">Received Comments</p>
+                <p className="text-gray">0</p>
               </div>
               <div>
-                <p className="text-gray-400">Received Upvotes</p>
-                <p className="font-bold">0</p>
+                <p className="font-bold">Received Upvotes</p>
+                <p className="text-gray">0</p>
               </div>
-              <button className="w-full bg-gray-700 py-2 rounded-lg">
+              <button className="w-full bar-background-color py-2 rounded-lg">
                 Please Login
               </button>
             </div>
