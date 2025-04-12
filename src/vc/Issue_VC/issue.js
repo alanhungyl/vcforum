11cc22ebe7948460f44597fac1ca3ff8d1016234
xkartana <hungykl1605@gmail.com>
2025-04-12 15:37:47 +0800

    basically done

--- src/vc/Issue_VC/issue.js
+++ src/vc/Issue_VC/issue.js
@@ -4,32 +4,32 @@ let data = JSON.stringify({
   "credentialDefinitionId": "1af0c77e-0dda-49f8-a7af-5cdfed403846",
   "credential": {
     "claims": {
-      "fullName": "Chan Siu Ming 陳小明",
+      "fullName": "Test",
       "gender": "Male",
-      "studentID": "1155123123",
-      "major": "BSc Computer Science",
-      "college": "United College",
-      "admissionYear": "2022"
+      "studentID": "1155456456",
+      "major": "BEng Engineering",
+      "college": "CC College",
+      "admissionYear": "2023"
     }
   }
 });
 
 let config = {
   method: 'post',
-maxBodyLength: Infinity,
+  maxBodyLength: Infinity,
   url: 'https://demo-api.igrant.io/v2/config/digital-wallet/openid/sdjwt/credential/issue',
-  headers: { 
-    'Content-Type': 'application/json', 
-    'Accept': 'application/json', 
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
     'Authorization': 'ApiKey eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTY29wZXMiOlsiY29uZmlnIiwiYXVkaXQiLCJzZXJ2aWNlIiwib25ib2FyZCJdLCJPcmdhbmlzYXRpb25JZCI6IjY2NTViOTg2MmUyYzMxMWI3YzM1MDY1NiIsIk9yZ2FuaXNhdGlvbkFkbWluSWQiOiI2NjU1Yjk4NTJlMmMzMTFiN2MzNTA2NTEiLCJEYXRhVmVyaWZpZXJVc2VySWQiOiIiLCJFbnYiOiIiLCJleHAiOjE3NDU0MDYzMzh9.0odMtlLY3fb1T1dcJejTlWOzNObPV4ZUaod6KTCpZGc'
   },
-  data : data
+  data: data
 };
 
 axios(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
